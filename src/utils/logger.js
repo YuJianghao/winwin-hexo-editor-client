af0b8e1ce96679524f6f293fc260aeb20043784a
winwin <>
2020-05-23 02:54:04 +0800

    add logger

---
+++ src/utils/logger.js
@@ -0,0 +1,58 @@
+const logLevel = ['debug', 'log', 'warn', 'error']
+const logLevelHas = (level) => {
+  return logLevel.indexOf(level) >= 0
+}
+
+class Logger {
+  constructor ({ level = process.env.NODE_ENV === 'production' ? 'error' : 'debug', prefix }) {
+    this.level = level
+    this.prefix = prefix || level
+    this.methodToColorMap = {
+      debug: '#7f8c8d', // Gray
+      log: '#2ecc71', // Green
+      warn: '#f39c12', // Yellow
+      error: '#c0392b', // Red
+      groupCollapsed: '#3498db', // Blue
+      groupEnd: null // No colored prefix on groupEnd
+    }
+    for (const method of Object.keys(this.methodToColorMap)) {
+      this[method] = (...args) => {
+        this._print(method, args)
+      }
+    }
+  }
+
+  _print (method, args) {
+    const styles = [
+  `background: ${this.methodToColorMap[method]}`,
+  'border-radius: 0.5em',
+  'color: white',
+  'font-weight: bold',
+  'padding: 2px 0.5em'
+    ]
+    const logPrefix = ['%c' + this.prefix, styles.join(';')]
+    if (this._shouldDo(method)) {
+      console[method](...logPrefix, ...args)
+    }
+  }
+
+  setLogLevel (level) {
+    if (!logLevelHas(level)) return false
+    else {
+      this.level = level
+    }
+  }
+
+  _shouldDo (method) {
+    if (logLevelHas(method)) {
+      console.debug('123')
+      console.log(method)
+      console.log(logLevel.indexOf(method) >= logLevel.indexOf(this.level))
+      return logLevel.indexOf(method) >= logLevel.indexOf(this.level)
+    } else {
+      return true
+    }
+  }
+}
+
+export { Logger }
