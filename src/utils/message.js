b0b5180753ebba5c3c114faca10642e7f22277df
winwin <>
2020-05-23 09:11:39 +0800

    添加message注释

--- src/utils/message.js
+++ src/utils/message.js
@@ -1,5 +1,11 @@
 import { Notify } from 'quasar'
 export default {
+  /**
+   *
+   * @param {object} opt 参数
+   * @param {string} message 消息内容
+   * @param {string} position 消息位置
+   */
   info (opt) {
     Notify.create(Object.assign({ ...opt }, {
       color: 'primary',
@@ -7,6 +13,12 @@ export default {
       timeout: 2000
     }))
   },
+  /**
+   *
+   * @param {object} opt 参数
+   * @param {string} message 消息内容
+   * @param {string} position 消息位置
+   */
   warning (opt) {
     Notify.create(Object.assign({ ...opt }, {
       color: 'primary',
@@ -14,12 +26,24 @@ export default {
       timeout: 2000
     }))
   },
+  /**
+   *
+   * @param {object} opt 参数
+   * @param {string} message 消息内容
+   * @param {string} position 消息位置
+   */
   success (opt) {
     Notify.create(Object.assign({ ...opt }, {
       color: 'primary',
       icon: 'done'
     }))
   },
+  /**
+   *
+   * @param {object} opt 参数
+   * @param {string} message 消息内容
+   * @param {string} position 消息位置
+   */
   error (opt) {
     Notify.create(Object.assign({ ...opt }, {
       color: 'red',
