194e1dc5c724abb53593f6e470b19c794e549edf
winwin <>
2020-05-24 11:14:34 +0800

    add pinyin support

--- src/store/editorSorter/getters.js
+++ src/store/editorSorter/getters.js
@@ -2,11 +2,18 @@
 export function someGetter (state) {
 }
 */
+const pinyin = require('pinyin')
 export function postsList (state, getter, rootState, rootGetters) {
   let postsList = rootGetters['editorFilter/postsList']
   postsList = postsList.sort((a, b) => {
     if (typeof a[state.key] === 'undefined') return -1
-    return (a[state.key] > b[state.key]) ^ !state.direction ? 1 : -1
+    let valueA = a[state.key]
+    let valueB = b[state.key]
+    if (typeof valueA === 'string') {
+      valueA = pinyin(valueA, { style: pinyin.STYLE_NORMAL }).join('')
+      valueB = pinyin(valueB, { style: pinyin.STYLE_NORMAL }).join('')
+    }
+    return valueA > valueB ^ !state.direction ? 1 : -1
   })
   return postsList
 }
