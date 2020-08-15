9ed6c088cc076dfa8d75cf1dcead8748282e371e
winwin <>
2020-08-16 00:48:17 +0800

    add filter mutation type

--- src/store/editorFilter/mutations.js
+++ src/store/editorFilter/mutations.js
@@ -1,26 +1,26 @@
-/*
-export function someMutation (state) {
+import * as mutationTypes from './mutation-types'
+const mutations = {
+  [mutationTypes.setFilterType]: (state, type) => {
+    state.type = type
+  },
+  [mutationTypes.setFilterId]: (state, _id) => {
+    state._id = _id
+  },
+  [mutationTypes.filterByAll]: (state) => {
+    mutations[mutationTypes.setFilterType](state, 'all')
+    mutations[mutationTypes.setFilterId](state, null)
+  },
+  [mutationTypes.filterByCategoriesId]: (state, _id) => {
+    mutations[mutationTypes.setFilterType](state, 'categories')
+    mutations[mutationTypes.setFilterId](state, _id)
+  },
+  [mutationTypes.filterByTagsId]: (state, _id) => {
+    mutations[mutationTypes.setFilterType](state, 'tags')
+    mutations[mutationTypes.setFilterId](state, _id)
+  },
+  [mutationTypes.filterByUnCategorized]: (state) => {
+    mutations[mutationTypes.setFilterType](state, 'uncategorized')
+    mutations[mutationTypes.setFilterId](state, null)
+  }
 }
-*/
-export function setFilterType (state, type) {
-  state.type = type
-}
-export function setFilterId (state, _id) {
-  state._id = _id
-}
-export function filterByAll (state) {
-  setFilterType(state, 'all')
-  setFilterId(state, null)
-}
-export function filterByCategoriesId (state, _id) {
-  setFilterType(state, 'categories')
-  setFilterId(state, _id)
-}
-export function filterByTagsId (state, _id) {
-  setFilterType(state, 'tags')
-  setFilterId(state, _id)
-}
-export function filterByUnCategorized (state) {
-  setFilterType(state, 'uncategorized')
-  setFilterId(state, null)
-}
+export default mutations
