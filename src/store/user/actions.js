5fa71128851c7d9c94490a0cbca41b34a3ff0c53
winwin_2011 <yjh2011@live.com>
2021-02-11 21:51:19 +0800

    fix(login):修复登录逻辑

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -1,11 +1,15 @@
+import { LocalStorage } from "quasar"
 import api from "src/api"
+import Router from "src/router"
+import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from "src/utils/constants"
 import { failedInfo } from "./mutations"
 
 export async function login({ commit }, { name, pass }) {
   commit('requestLogin')
   try {
-    api.auth.login(name, pass)
+    await api.auth.login(name, pass)
     commit('successLogin')
+    Router.push('/')
   } catch (err) {
     commit('failedLogin', err)
   }
@@ -13,8 +17,11 @@ export async function login({ commit }, { name, pass }) {
 
 export async function logout({ commit }, local) {
   try {
-    if (!local) api.auth.logout()
+    if (!local) await api.auth.logout()
+    LocalStorage.remove(ACCESS_TOKEN_KEY)
+    LocalStorage.remove(REFRESH_TOKEN_KEY)
     commit('logout')
+    Router.push('/login')
   } catch (e) { }
 }
 
