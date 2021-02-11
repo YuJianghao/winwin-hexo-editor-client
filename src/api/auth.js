a2d3be97323f6d95572430e32dc345224fdb6300
winwin_2011 <yjh2011@live.com>
2021-02-11 16:02:11 +0800

    clean:移除旧代码

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,6 @@ import { SHA1 } from 'crypto-js'
 import axios from 'axios'
 import { LocalStorage } from 'quasar'
 import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from 'src/utils/constants'
-import { request, refresh } from './request'
 export default {
   login: async (name, pass) => {
     const res = await axios.post((process.env.DEV ? '/api' : window.location.origin) + '/login', {}, {
@@ -16,11 +15,11 @@ export default {
     return res.data
   },
   logout: async () => {
-    return request.get('/pages')
+    return axios.get('/pages')
   },
   refresh: async () => {
-    const res = await refresh.post('/refresh')
-    LocalStorage.set(ACCESS_TOKEN_KEY, res.data.accessToken)
-    LocalStorage.set(REFRESH_TOKEN_KEY, res.data.refreshToken)
+    const res = await axios.post('/refresh')
+    LocalStorage.set(ACCESS_TOKEN_KEY, res.data.data.accessToken)
+    LocalStorage.set(REFRESH_TOKEN_KEY, res.data.data.refreshToken)
   },
 }
--- src/api/hexo.js
+++ src/api/hexo.js
@@ -1,16 +1,16 @@
-import { request } from './request'
+import { axios } from 'axios'
 export default {
   listPosts: async () => {
-    return request.get('/posts')
+    return axios.get('/posts')
   },
   listPages: async () => {
-    return request.get('/pages')
+    return axios.get('/pages')
   },
   listTags: async () => {
-    return request.get('/tags')
+    return axios.get('/tags')
   },
   listCategories: async () => {
-    return request.get('/categories')
+    return axios.get('/categories')
   },
   newPostOrPage: async (title, opt = {}) => {
     const { layout, path, slug, replace } = opt
@@ -18,12 +18,12 @@ export default {
     new Array([layout, path, slug, replace]).map(key => {
       if (key !== undefined) { data[key] = key }
     })
-    return request.post('/new', data)
+    return axios.post('/new', data)
   },
   updatePostOrPage: async (id, page, obj) => {
-    return request.post('/update', { id, page, obj })
+    return axios.post('/update', { id, page, obj })
   },
   deletePostOrPage: async (id, page) => {
-    return request.post('/delete', { id, page })
+    return axios.post('/delete', { id, page })
   },
 }
