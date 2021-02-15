8c62471e0dd609667b092433eac9de9adbcf2e00
winwin_2011 <yjh2011@live.com>
2021-02-16 00:56:25 +0800

    feat(editor):禁用用户输入，使用选择器

--- src/components/Editors/UpdatedEditor.vue
+++ src/components/Editors/UpdatedEditor.vue
@@ -1,50 +1,60 @@
 <template>
-  <q-item>
+  <q-item clickable>
     <q-item-section>
       <q-item-label caption>
         更新于
       </q-item-label>
       <q-item-label class="row no-wrap">
-        <m-input
-          v-model="updated"
-          :error="error"
-          class="col"
-          mask="####-##-## ##:##:##"
-          clearable
-        ></m-input>
-        <q-btn size="x-small" icon="date_range" :ripple="false" round>
-          <q-menu>
-            <div class="row">
-              <q-date
-                v-model="updated"
-                :mask="format"
-                color="primary"
-                class="no-shadow"
-              />
-              <q-time
-                v-model="updated"
-                :mask="format"
-                color="primary"
-                class="no-shadow"
-                format24h
-              />
-            </div>
-          </q-menu>
-        </q-btn>
+        {{ updated || "未指定数据" }}
       </q-item-label>
     </q-item-section>
+    <q-item-section side>
+      <q-btn
+        size="x-small"
+        icon="clear"
+        :ripple="false"
+        round
+        @click.stop="updated = ''"
+        v-if="updated"
+      ></q-btn>
+      <q-btn
+        size="x-small"
+        icon="today"
+        :ripple="false"
+        round
+        @click.stop="now"
+        v-else
+        ><q-tooltip>
+          指定为现在
+        </q-tooltip>
+      </q-btn>
+    </q-item-section>
+    <q-menu>
+      <div class="row">
+        <q-date
+          v-model="updated"
+          :mask="format"
+          color="primary"
+          class="no-shadow"
+        />
+        <q-time
+          v-model="updated"
+          :mask="format"
+          color="primary"
+          class="no-shadow"
+          format24h
+        />
+      </div>
+    </q-menu>
   </q-item>
 </template>
 
 <script>
 import { DATE_FORMAT } from "src/utils/constants";
-import MInput from "../UI/MInput";
+import { date } from "quasar";
 export default {
   name: "UpdatedEditor",
   props: ["post", "getObj", "localUpdate"],
-  components: {
-    MInput
-  },
   computed: {
     format() {
       return DATE_FORMAT;
@@ -61,6 +71,11 @@ export default {
         obj.updated = v;
         this.localUpdate(obj);
       }
+    }
+  },
+  methods: {
+    now() {
+      this.updated = date.formatDate(new Date(), DATE_FORMAT);
     }
   }
 };
